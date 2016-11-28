a574ef52da6fdad85b3d1612732b33bb28982002
TruongSa <shrimp2t@gmail.com>
2016-11-28 21:31:04 +0700

    update

--- sa.php
+++ sa.php
@@ -1,7 +1,17 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: truongsa
- * Date: 11/28/16
- * Time: 9:23 PM
+ * Plugin Name:      Sa Test Plugtin
+ * Plugin URI:        http:shrimp2t.com/
+ * Description:      Test sub repo
+ * Version:           1.0.0
+ * Author:            SmoothThemes
+ * Author URI:        http:shrimp2t.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wpu
+ * Domain Path:       /languages
  */
+
+
+require_once dirname( __FILE__ ).'/Lib/wp-screen-info-debug.php';
